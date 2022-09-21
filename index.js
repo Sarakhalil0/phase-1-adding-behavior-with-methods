1e2ee664ca38f075934dd8b38893e53912fd4590
sara khalil <sarakhalil3377@gmail.com>
2022-09-21 11:12:11 +0300

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+//first
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+}
+const cat = new Cat('ted', 'female')
+
+//second
+class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+
+}
+const dog = new Dog('bob', 'male')
+
+//third
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex === 'male') {
+            return ` It's me! ${this.name}, the parrot!`
+        } else {
+            return `${this.name} says squawk!`
+        }
+
+    }
+}
+const bird = new Bird('parrot', 'male')
+//bird.speak()
+
